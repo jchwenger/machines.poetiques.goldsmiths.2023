39b1262c31e0c2da4ff0d3e6a7c90822cfe9d17c
jchwenger <34098722+jchwenger@users.noreply.github.com>
2023-05-11 23:21:25 +0100

    cinema |

---
+++ garnier-pierre-cinema/sketch.js
@@ -0,0 +1,70 @@
+// --------------------------------------------------------------------------------
+// Pierre Garnier, 'Dalle funéraire spatialiste', from *Cimetière spatialiste*, 1973
+//
+// Jérémie Wenger, 2023
+// --------------------------------------------------------------------------------
+
+// a very interesting article (in French, unfortunately) with many a beautiful
+// and inspirational example of visual poetry
+// https://journals.openedition.org/interfaces/2009?lang=en
+
+let word;
+let repeatedWord;
+let font;
+
+let xInit;
+let yInit;
+let horizontalRepeat;
+let jaggedScheme;
+let charWidth;
+
+function setup() {
+  createCanvas(500, 700);
+
+  textFont('Courier');
+  textSize(10);
+  textLeading(9);
+  charWidth = textWidth('m');
+  fill(0);
+
+  horizontalRepeat = 6
+
+  word  = "cinéma";
+  repeatedWord = word.repeat(horizontalRepeat);
+
+  xInit = 120;
+  yInit = 70;
+
+  jaggedScheme = [
+    { 'length': 13, 'direction': 'right' },
+    { 'length': 7, 'direction': 'left' },
+    { 'length': 4, 'direction': 'right' },
+    { 'length': 11, 'direction': 'left' },
+    { 'length': 9, 'direction': 'right' },
+    { 'length': 7, 'direction': 'left' },
+    { 'length': 8, 'direction': 'right' },
+  ];
+}
+
+function draw() {
+  background(233, 232, 227);
+
+  // draw the main text
+  xCurrent = xInit;
+  yCurrent = yInit;
+  for (scheme of jaggedScheme) {
+    for (let i = 0; i < scheme.length; i++) {
+      text(repeatedWord, xCurrent, yCurrent); // .repeat: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+      // move one char left or right
+      if (scheme.direction === 'right') {
+        xCurrent += charWidth;
+      } else if (scheme.direction === 'left'){
+        xCurrent -= charWidth;
+      }
+      yCurrent += textLeading(); // next line
+    }
+  }
+
+  // draw the comment
+  text('Dalle funéraire spatialiste', 250, height - 50);
+}
