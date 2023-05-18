d3eb719666fe597bb53f627c2f10f1c553f445eb
jchwenger <34098722+jchwenger@users.noreply.github.com>
2023-05-18 12:35:44 +0100

    cinema | hints

--- garnier-pierre-cinema/sketch.js
+++ garnier-pierre-cinema/sketch.js
@@ -10,6 +10,18 @@
 // and inspirational example of visual poetry
 // https://journals.openedition.org/interfaces/2009?lang=en
 
+// Ideas for further development:
+// - if you wanted to make some of the occurrences of the word disappear, like
+//   in 'silencio', how would you go about doing that? (Hint: you would have to
+//   implement a loop that draws a line by writing words one after the other,
+//   instead of printing a repeated string as I did. Another option would be to
+//   change the way you prepare the prepared string, by adding spaces (or other
+//   characters) instead of the word at the desired location.
+// - maybe you would like to add movement to the sketch? A nice trick for
+//   oscillations is to use Math.sin(frameCount) or Math.cos(frameCount),
+//   tweaking frequency (speed) like so: Math.sin(frameCount/freq); and the size
+//   of the resulting number like so: Math.sin(frameCount) * scaling
+
 let word;
 let repeatedWord;
 let font;
@@ -32,11 +44,14 @@ function setup() {
   horizontalRepeat = 6
 
   word  = "cinéma";
-  repeatedWord = word.repeat(horizontalRepeat);
+  repeatedWord = word.repeat(horizontalRepeat); // .repeat: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 
   xInit = 120;
   yInit = 70;
 
+  // TODO: do you need this 'direction' attribute, if one only ever goes the
+  // opposite direction at each section (right always after left, left always
+  // after right) ?
   jaggedScheme = [
     { 'length': 13, 'direction': 'right' },
     { 'length': 7, 'direction': 'left' },
@@ -56,7 +71,7 @@ function draw() {
   yCurrent = yInit;
   for (scheme of jaggedScheme) {
     for (let i = 0; i < scheme.length; i++) {
-      text(repeatedWord, xCurrent, yCurrent); // .repeat: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+      text(repeatedWord, xCurrent, yCurrent);
       // move one char left or right
       if (scheme.direction === 'right') {
         xCurrent += charWidth;
