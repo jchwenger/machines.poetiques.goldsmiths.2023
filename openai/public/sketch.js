157a16ae72450fc7c156eca9819414202689af4a
jchwenger <34098722+jchwenger@users.noreply.github.com>
2023-05-19 13:15:20 +0100

    openai | functional server & ui for completions, chat & images

--- openai/public/sketch.js
+++ openai/public/sketch.js
@@ -6,39 +6,258 @@
 // systems through experimental French poetry, Goldsmiths College
 // --------------------------------------------------------------------------------
 
+const margin = 20;
+
 const socket = io();
 
 function setup() {
-  createCanvas(700, 400);
+  createCanvas(400, 700);
   textFont("Helvetica");
   textSize(20);
   fill(0);
   textAlign(CENTER);
   // const response = requestText();
   // console.log(response);
+  createUI();
+  background(255, 254, 242);
+
 }
 
 function draw() {
-  background(95, 250, 67);
-  // text(response, 20, 20);
-  // console.log(response);
-  text("BLAH", width/2, height/2);
+
 }
 
 function keyPressed() {
-  if (key === ' ') {
-    console.log('requesting!');
-    socket.emit('chat message', 'blah', (response) => console.log(response));
+  if (key === '¬') {
+    toggleUI();
   }
 }
 
-socket.on('message', (msg) => console.log(msg));
+// --------------------------------------------------------------------------------
+// display functions
 
-// requestText = async () => {
-//    return await openai.createCompletion({
-//     model: "text-davinci-003",
-//     prompt: "Say this is a test",
-//     temperature: 0,
-//     max_tokens: 7,
-//   });
-// }
+function displayResponse(message) {
+
+  background(255, 254, 242);
+
+  fill(0);
+  textSize(14);
+  textAlign(LEFT);
+  textFont('Georgia');
+
+  text(message, margin, margin, width - 2 * margin);
+}
+
+function displayImage(message) {
+
+  background(255, 254, 242);
+
+  // console.log(`inside displayImage, message:`);
+  // console.log(message);
+  const img = createImg(message.url); // not the same as createImage!
+  img.position(margin, margin);
+}
+
+// --------------------------------------------------------------------------------
+// request functions
+
+function requestCompletion() {
+  const form = document.getElementById('request-form');
+  const data = new FormData(form);
+  console.log('requesting completion');
+  socket.emit('completion request', {
+      'prompt': data.get('prompt'),
+      'max_tokens': data.get('max-tokens'),
+      'temperature': data.get('temperature'),
+    }, (response) => console.log(response));
+}
+
+function requestChat() {
+  const form = document.getElementById('request-form');
+  const data = new FormData(form);
+  console.log('requesting chat');
+  console.log(form);
+  socket.emit('chat request', {
+    'prompt': data.get('prompt'),
+    'system_prompt': data.get('system'),
+    'max_tokens': data.get('max-tokens'),
+    'temperature': data.get('temperature'),
+  }, (response) => console.log(response));
+}
+
+function requestImage() {
+  const form = document.getElementById('request-form');
+  const data = new FormData(form);
+  console.log('requesting image');
+  console.log(form);
+    socket.emit('image request', {
+      'prompt': data.get('prompt'),
+    }, (response) => console.log(response));
+}
+
+// --------------------------------------------------------------------------------
+// interact with server / make requests to OpenAI
+
+socket.on('completion response', (message) => {
+  console.log('completion response:');
+  console.log(message);
+  displayResponse(message);
+});
+
+socket.on('chat response', (message) => {
+  console.log('chat response:');
+  console.log(message);
+  displayResponse(message);
+});
+
+socket.on('image response', (message) => {
+  console.log('image response:');
+  console.log(message);
+  displayImage(message);
+});
+
+// --------------------------------------------------------------------------------
+// UI with text boxes, buttons, etc.
+// partial inspiration from here: https://www.geeksforgeeks.org/how-to-create-a-form-dynamically-with-the-javascript/
+
+function createUI() {
+
+  const UI = document.createElement("div");
+  UI.setAttribute("id", "ui");
+
+  const title = document.createElement("h1");
+  title.innerHTML = "Control panel";
+
+  // form
+  const form = document.createElement("form");
+  form.setAttribute("id", "request-form");
+
+  // base prompt ---------------------------------------
+  const promptDiv = document.createElement('div');
+  promptDiv.setAttribute("id", "textarea-container");
+
+  const prompt = document.createElement("textarea");
+  prompt.setAttribute("type", "text");
+  prompt.setAttribute("name", "prompt");
+  prompt.setAttribute("rows", "10");
+  prompt.setAttribute("cols", "50");
+
+  const promptLabel = document.createElement("label");
+  promptLabel.innerHTML = "prompt";
+
+  promptDiv.appendChild(promptLabel);
+  promptDiv.appendChild(prompt);
+
+  // system prompt -------------------------------------
+  const systemDiv = document.createElement('div');
+  systemDiv.setAttribute("id", "textarea-container");
+
+  const system = document.createElement("textarea");
+  system.setAttribute("type", "text");
+  system.setAttribute("name", "system");
+  system.setAttribute("rows", "10");
+  system.setAttribute("cols", "50");
+
+  const systemLabel = document.createElement("label");
+  systemLabel.innerHTML = "system prompt";
+
+  systemDiv.appendChild(systemLabel);
+  systemDiv.appendChild(system);
+
+  // parameters ----------------------------------------
+  const parametersDiv = document.createElement('div');
+  parametersDiv.setAttribute("id", "parameters-container");
+
+  // temperature
+  const temperature = document.createElement("input");
+  temperature.setAttribute("type", "number");
+  temperature.setAttribute("min", "0.1");
+  temperature.setAttribute("max", "2.0");
+  temperature.setAttribute("step", "0.1");
+  temperature.setAttribute("name", "temperature");
+  temperature.setAttribute("value", "0.7");
+  temperature.setAttribute("placeholder", "0.7");
+
+  const temperatureLabel = document.createElement("label");
+  temperatureLabel.innerHTML = "temperature";
+
+  const maxTokens = document.createElement("input");
+  maxTokens.setAttribute("type", "number");
+  maxTokens.setAttribute("min", "1");
+  maxTokens.setAttribute("max", "256");
+  maxTokens.setAttribute("step", "1");
+  maxTokens.setAttribute("name", "max-tokens");
+  maxTokens.setAttribute("value", "7");
+  maxTokens.setAttribute("placeholder", "7");
+
+  const maxTokensLabel = document.createElement("label");
+  maxTokensLabel.innerHTML = "max tokens";
+
+  parametersDiv.appendChild(maxTokensLabel);
+  parametersDiv.appendChild(maxTokens);
+  parametersDiv.appendChild(temperatureLabel);
+  parametersDiv.appendChild(temperature);
+
+  // buttons -------------------------------------------
+  const buttonsDiv = document.createElement("div");
+  buttonsDiv.setAttribute("id", "buttons");
+
+  // completion button
+  const completionButton = document.createElement("button");
+  completionButton.setAttribute("type", "button");
+  completionButton.setAttribute("content", "completion");
+  completionButton.setAttribute("id", "completion-button");
+  completionButton.textContent = "completion";
+  completionButton.addEventListener("click", () => requestCompletion());
+
+  // chat button
+  const chatButton = document.createElement("button");
+  chatButton.setAttribute("type", "button");
+  chatButton.setAttribute("content", "chat");
+  completionButton.setAttribute("id", "chat-button");
+  chatButton.textContent = "chat";
+  chatButton.addEventListener("click", () => requestChat());
+
+  // img button
+  const imgButton = document.createElement("button");
+  imgButton.setAttribute("type", "button");
+  imgButton.setAttribute("content", "image");
+  completionButton.setAttribute("id", "img-button");
+  imgButton.textContent = "image";
+  imgButton.addEventListener("click", () => requestImage());
+
+  // reset button
+  const resetButton = document.createElement("button");
+  resetButton.setAttribute("type", "reset");
+  resetButton.setAttribute("content", "reset");
+  resetButton.textContent = "reset";
+
+  buttonsDiv.appendChild(completionButton);
+  buttonsDiv.appendChild(chatButton);
+  buttonsDiv.appendChild(imgButton);
+  buttonsDiv.appendChild(resetButton);
+
+  // build form & ui -----------------------------------
+
+  // append the elements to the form
+  form.appendChild(promptDiv);
+  form.appendChild(systemDiv);
+  form.appendChild(parametersDiv);
+  form.appendChild(buttonsDiv);
+
+  // append the form to the body
+  UI.appendChild(title);
+  UI.appendChild(form);
+
+  // append to the body
+  document.body.appendChild(UI);
+}
+
+function toggleUI() {
+  const ui = document.getElementById('ui');
+  if (ui.style.display === 'none') {
+    ui.style.display = 'block';
+  } else {
+    ui.style.display = 'none';
+  }
+}
--- openai/server.js
+++ openai/server.js
@@ -6,17 +6,41 @@
 // systems through experimental French poetry, Goldsmiths College
 // --------------------------------------------------------------------------------
 
-// const { Configuration, OpenAIApi } = require('openai');
-// const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-// const openai = new OpenAIApi(configuration);
-// const response = await openai.createCompletion({
-//   model: 'text-davinci-003',
-//   prompt: 'Say this is a test',
-//   temperature: 0,
-//   max_tokens: 7,
-// });
+const { Configuration, OpenAIApi } = require('openai');
+const fs = require('fs');
+
+// Authentication, two ways: or a file
+// 1) local environment
+//    for this, you need to define the variable in your terminal before launching node:
+//    export OPENAI_API_KEY=...
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+// // 2)
+// // Synchronous file read, cf. https://nodejs.dev/en/learn/reading-files-with-nodejs/ (and GPT :>)
+// function configureAPI() {
+//   try {
+//     const data = fs.readFileSync(__dirname + '/secret.txt');
+//     // console.log(`My secret key: ${data.toString().trim()}`);
+//     return new Configuration({
+//       apiKey: data.toString().trim(),
+//     });
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+// const configuration = configureAPI();
+
+const openai = new OpenAIApi(configuration);
+
+// async function listEngines() {
+//   return await openai.listEngines();
+// }
+// console.log('requesting engines');
+// const response = listEngines();
+// console.log(response);
+// console.log('done');
 
 // https://socket.io/get-started/chat#integrating-socketio
 // See also Dan Shiffman's tutorial on Node & Websockets for more information
@@ -42,9 +66,109 @@ io.on('connection', (socket) => {
   io.emit('message', 'hello');
   // console.log('sent message');
 
-  socket.on('request', (message) => {
-    console.log(`chat message! ${message}`);
-    io.emit('message', 'diblah');
+  socket.on('completion request', (message, sock) => {
+    console.log(`completion requested by user`);
+    console.log(message);
+    sock('the server received your completion request');
+    console.log('making request to the model');
+    requestCompletion(...Object.values(message))
+      .then((response) => {
+        // console.log(response); // see the full horror of the response object
+        console.log(response.data.choices);
+        const t = response.data.choices[0].text // TODO: OpenAI gives the option to get multiple responses for one request, to be explored!
+        console.log('it answered!');
+        io.emit('completion response', t);
+      })
+      .catch((e) => {
+        io.emit('completion response', "");
+        console.error(e);
+      });
+  });
+
+  socket.on('chat request', (message, sock) => {
+    console.log(`chat requested by user`);
+    console.log(message);
+    sock('the server received your chat request');
+    console.log('making request to the model');
+    requestMessage(...Object.values(message))
+      .then((response) => {
+        // console.log(response); // see the full horror of the response object
+        console.log(response.data.choices);
+        const t = response.data.choices[0].message.content;  // TODO: OpenAI gives the option to get multiple responses for one request, to be explored!
+        console.log('it answered!');
+        console.log(response.data.choices);
+        io.emit('chat response', t);
+      })
+      .catch((e) => {
+        io.emit('chat response', "");
+        console.error(e);
+      });
+  });
+
+  socket.on('image request', (message, sock) => {
+    console.log(`image requested by user`);
+    console.log(message);
+    sock('the server received your image request');
+    console.log('making request to the model');
+    requestImage(...Object.values(message))
+      .then((response) => {
+        // console.log(response); // see the full horror of the response object
+        const t = response.data.data[0];  // TODO: OpenAI gives the option to get multiple images for one request, to be explored!
+        console.log('it answered!');
+        console.log(response.data);
+        io.emit('image response', t);
+      })
+      .catch((e) => {
+        io.emit('image response', "");
+        console.error(e);
+      });
   });
 
 });
+
+// separate async function (required if we want to use the await keyword)
+// this allows us to make a call to the API, and wait for it to respond
+// without breaking our code
+async function requestCompletion(
+  prompt = 'Say this is a test',
+  max_tokens = 7,
+  temperature = 0.7,
+) {
+  console.log('inside requestCompletion', prompt, max_tokens, temperature);
+  return await openai.createCompletion({
+    model: 'text-davinci-003', // TODO: search the documentation for various models, possibly allow the user to change this from the UI
+    prompt: prompt,
+    temperature: parseFloat(temperature),  // security checks
+    max_tokens: parseInt(max_tokens),      // for the variable type
+    n: 1, // TODO: the parameter for requesting more than one answer
+  });
+}
+
+async function requestMessage(
+  prompt = 'Say this is a test',
+  system_prompt = 'You are William Shakespeare and speak like in the 1590s.',
+  max_tokens = 7,
+  temperature = 0.7,
+) {
+  console.log('inside requestChat', prompt, system_prompt, max_tokens, temperature);
+  return await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {role: "system", content: system_prompt},
+      {role: "user", content: prompt}
+    ],
+    max_tokens: parseInt(max_tokens),     // security checks
+    temperature: parseFloat(temperature), // for the variable type
+    n: 1, // TODO: the parameter for requesting more than one answer
+  });
+}
+
+async function requestImage(
+  prompt = "The rainbow cyborg armageddon, Dutch oil painting, 1723, British Museum"
+) {
+  return await openai.createImage({
+    prompt: prompt,
+    n: 1,              // TODO: the number of images should be betwen 1 & 10 → add the appropriate field to the UI
+    size: "256x256",  // TODO: add a way for users to change image sizes, possible choices: 256x256, 512x512, or 1024x1024
+  });
+}
